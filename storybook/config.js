062e42128d622169c8e7e670dcccfdff9f65d8fc
Fernando Daciuk <f.daciuk@gmail.com>
2017-02-18 12:16:06 -0200

    Update storybook config

--- storybook/config.js
+++ storybook/config.js
@@ -1,4 +1,8 @@
-import { configure } from '@kadira/storybook'
+import { configure, setAddon } from '@kadira/storybook'
+import addonInfo from '@kadira/react-storybook-addon-info'
+import 'milligram'
+
+setAddon(addonInfo)
 
 const req = require.context('../src/components', true, /\.story\.js$/)
 
